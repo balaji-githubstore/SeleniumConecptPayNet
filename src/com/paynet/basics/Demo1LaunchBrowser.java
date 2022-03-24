3ec83a23019ed98131c15a81e472d7fc42319c2d
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-03-24 12:48:47 +0530

    added comment

--- src/com/paynet/basics/Demo1LaunchBrowser.java
+++ src/com/paynet/basics/Demo1LaunchBrowser.java
@@ -2,7 +2,7 @@ package com.paynet.basics;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-
+//demo1
 public class Demo1LaunchBrowser {
 
 	public static void main(String[] args) {
